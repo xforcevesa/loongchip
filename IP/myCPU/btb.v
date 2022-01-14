a4c8014fc4033e5331d10758c28dad896e2c0bc4
chenzeshuai <chenzeshuai@loongson.cn>
2022-01-14 13:07:40 +0800

    btb imple error

--- IP/myCPU/btb.v
+++ IP/myCPU/btb.v
@@ -149,38 +149,38 @@ always @(posedge clk) begin
     end
 end
 
-assign {match_target, match_counter, match_index, match_jirl_flag} = {37{match_rd[0 ]}} & {target[0 ], counter[0 ], 5'd0 , jirl_flag[0 ]} |
-                                                                     {37{match_rd[1 ]}} & {target[1 ], counter[1 ], 5'd1 , jirl_flag[1 ]} |
-                                                                     {37{match_rd[2 ]}} & {target[2 ], counter[2 ], 5'd2 , jirl_flag[2 ]} |
-                                                                     {37{match_rd[3 ]}} & {target[3 ], counter[3 ], 5'd3 , jirl_flag[3 ]} |
-                                                                     {37{match_rd[4 ]}} & {target[4 ], counter[4 ], 5'd4 , jirl_flag[4 ]} |
-                                                                     {37{match_rd[5 ]}} & {target[5 ], counter[5 ], 5'd5 , jirl_flag[5 ]} |
-                                                                     {37{match_rd[6 ]}} & {target[6 ], counter[6 ], 5'd6 , jirl_flag[6 ]} |
-                                                                     {37{match_rd[7 ]}} & {target[7 ], counter[7 ], 5'd7 , jirl_flag[7 ]} |
-                                                                     {37{match_rd[8 ]}} & {target[8 ], counter[8 ], 5'd8 , jirl_flag[8 ]} |
-                                                                     {37{match_rd[9 ]}} & {target[9 ], counter[9 ], 5'd9 , jirl_flag[9 ]} |
-                                                                     {37{match_rd[10]}} & {target[10], counter[10], 5'd10, jirl_flag[10]} |
-                                                                     {37{match_rd[11]}} & {target[11], counter[11], 5'd11, jirl_flag[11]} |
-                                                                     {37{match_rd[12]}} & {target[12], counter[12], 5'd12, jirl_flag[12]} |
-                                                                     {37{match_rd[13]}} & {target[13], counter[13], 5'd13, jirl_flag[13]} |
-                                                                     {37{match_rd[14]}} & {target[14], counter[14], 5'd14, jirl_flag[14]} |
-                                                                     {37{match_rd[15]}} & {target[15], counter[15], 5'd15, jirl_flag[15]} |
-                                                                     {37{match_rd[16]}} & {target[16], counter[16], 5'd16, jirl_flag[16]} |
-                                                                     {37{match_rd[17]}} & {target[17], counter[17], 5'd17, jirl_flag[17]} |
-                                                                     {37{match_rd[18]}} & {target[18], counter[18], 5'd18, jirl_flag[18]} |
-                                                                     {37{match_rd[19]}} & {target[19], counter[19], 5'd19, jirl_flag[19]} |
-                                                                     {37{match_rd[20]}} & {target[20], counter[20], 5'd20, jirl_flag[20]} |
-                                                                     {37{match_rd[21]}} & {target[21], counter[21], 5'd21, jirl_flag[21]} |
-                                                                     {37{match_rd[22]}} & {target[22], counter[22], 5'd22, jirl_flag[22]} |
-                                                                     {37{match_rd[23]}} & {target[23], counter[23], 5'd23, jirl_flag[23]} |
-                                                                     {37{match_rd[24]}} & {target[24], counter[24], 5'd24, jirl_flag[24]} |
-                                                                     {37{match_rd[25]}} & {target[25], counter[25], 5'd25, jirl_flag[25]} |
-                                                                     {37{match_rd[26]}} & {target[26], counter[26], 5'd26, jirl_flag[26]} |
-                                                                     {37{match_rd[27]}} & {target[27], counter[27], 5'd27, jirl_flag[27]} |
-                                                                     {37{match_rd[28]}} & {target[28], counter[28], 5'd28, jirl_flag[28]} |
-                                                                     {37{match_rd[29]}} & {target[29], counter[29], 5'd29, jirl_flag[29]} |
-                                                                     {37{match_rd[30]}} & {target[30], counter[30], 5'd30, jirl_flag[30]} |
-                                                                     {37{match_rd[31]}} & {target[31], counter[31], 5'd31, jirl_flag[31]};
+assign {match_target, match_counter, match_index, match_jirl_flag} = {39{match_rd[0 ]}} & {target[0 ], counter[0 ], 5'd0 , jirl_flag[0 ]} |
+                                                                     {39{match_rd[1 ]}} & {target[1 ], counter[1 ], 5'd1 , jirl_flag[1 ]} |
+                                                                     {39{match_rd[2 ]}} & {target[2 ], counter[2 ], 5'd2 , jirl_flag[2 ]} |
+                                                                     {39{match_rd[3 ]}} & {target[3 ], counter[3 ], 5'd3 , jirl_flag[3 ]} |
+                                                                     {39{match_rd[4 ]}} & {target[4 ], counter[4 ], 5'd4 , jirl_flag[4 ]} |
+                                                                     {39{match_rd[5 ]}} & {target[5 ], counter[5 ], 5'd5 , jirl_flag[5 ]} |
+                                                                     {39{match_rd[6 ]}} & {target[6 ], counter[6 ], 5'd6 , jirl_flag[6 ]} |
+                                                                     {39{match_rd[7 ]}} & {target[7 ], counter[7 ], 5'd7 , jirl_flag[7 ]} |
+                                                                     {39{match_rd[8 ]}} & {target[8 ], counter[8 ], 5'd8 , jirl_flag[8 ]} |
+                                                                     {39{match_rd[9 ]}} & {target[9 ], counter[9 ], 5'd9 , jirl_flag[9 ]} |
+                                                                     {39{match_rd[10]}} & {target[10], counter[10], 5'd10, jirl_flag[10]} |
+                                                                     {39{match_rd[11]}} & {target[11], counter[11], 5'd11, jirl_flag[11]} |
+                                                                     {39{match_rd[12]}} & {target[12], counter[12], 5'd12, jirl_flag[12]} |
+                                                                     {39{match_rd[13]}} & {target[13], counter[13], 5'd13, jirl_flag[13]} |
+                                                                     {39{match_rd[14]}} & {target[14], counter[14], 5'd14, jirl_flag[14]} |
+                                                                     {39{match_rd[15]}} & {target[15], counter[15], 5'd15, jirl_flag[15]} |
+                                                                     {39{match_rd[16]}} & {target[16], counter[16], 5'd16, jirl_flag[16]} |
+                                                                     {39{match_rd[17]}} & {target[17], counter[17], 5'd17, jirl_flag[17]} |
+                                                                     {39{match_rd[18]}} & {target[18], counter[18], 5'd18, jirl_flag[18]} |
+                                                                     {39{match_rd[19]}} & {target[19], counter[19], 5'd19, jirl_flag[19]} |
+                                                                     {39{match_rd[20]}} & {target[20], counter[20], 5'd20, jirl_flag[20]} |
+                                                                     {39{match_rd[21]}} & {target[21], counter[21], 5'd21, jirl_flag[21]} |
+                                                                     {39{match_rd[22]}} & {target[22], counter[22], 5'd22, jirl_flag[22]} |
+                                                                     {39{match_rd[23]}} & {target[23], counter[23], 5'd23, jirl_flag[23]} |
+                                                                     {39{match_rd[24]}} & {target[24], counter[24], 5'd24, jirl_flag[24]} |
+                                                                     {39{match_rd[25]}} & {target[25], counter[25], 5'd25, jirl_flag[25]} |
+                                                                     {39{match_rd[26]}} & {target[26], counter[26], 5'd26, jirl_flag[26]} |
+                                                                     {39{match_rd[27]}} & {target[27], counter[27], 5'd27, jirl_flag[27]} |
+                                                                     {39{match_rd[28]}} & {target[28], counter[28], 5'd28, jirl_flag[28]} |
+                                                                     {39{match_rd[29]}} & {target[29], counter[29], 5'd29, jirl_flag[29]} |
+                                                                     {39{match_rd[30]}} & {target[30], counter[30], 5'd30, jirl_flag[30]} |
+                                                                     {39{match_rd[31]}} & {target[31], counter[31], 5'd31, jirl_flag[31]};
 
 assign ret_pc = match_jirl_flag ? {ras_buffer, 2'b0} : {match_target, 2'b0};
 assign ret_en = |match_rd;
